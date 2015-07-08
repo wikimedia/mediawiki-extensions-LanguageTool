c457ee30a508b58c5fcb919f7e3485a6c8c7da37
ankita-ks <kumariankita002@gmail.com>
2015-07-09 00:55:33 +0530

    Removed extract function
    This function is redundant as text is now extracted within another function ('send').
    
    Change-Id: I2c862fbfa9719b93c94fc9cfd2573eb82a843ea4

--- modules/ext.LanguageToolAction.js
+++ modules/ext.LanguageToolAction.js
@@ -47,37 +47,9 @@ mw.languageToolAction.static.name = 'languageTool';
  * @static
  * @property
  */
-mw.languageToolAction.static.methods = [ 'extract', 'send' ];
+mw.languageToolAction.static.methods = [ 'send' ];
 
 /* Methods */
-
-/**
- * Extract text from all text nodes
- *
- * @method
- * @return {boolean} Action was executed
- */
-mw.languageToolAction.prototype.extract = function () {
-	var nodes = [];
-
-	function getTextNodes( obj ) {
-		var i;
-
-		for ( i = 0; i < obj.children.length; i++ ) {
-			if ( obj.children[ i ].type === 'text') {
-				nodes.push( obj.children[ i ] );
-			}
-
-			if ( obj.children[i].children ) {
-				getTextNodes( obj.children[ i ] );
-			}
-		}
-	}
-
-	getTextNodes( ve.init.target.getSurface().getModel().getDocument().getDocumentNode() );
-
-	return nodes;
-};
 
 /**
  * Send text to LanguageTool server
@@ -86,10 +58,9 @@ mw.languageToolAction.prototype.extract = function () {
  * @return {NULL} Action was executed
  */
 mw.languageToolAction.prototype.send = function () {
-	var textNodes, model, data, mapper, i, textArray, text, lang,
+	var model, data, mapper, i, textArray, text, lang,
 		self = this;
 
-	textNodes = this.extract();
 	model = ve.init.target.getSurface().getModel();
 
 	data = model.getDocument().data.getData();
