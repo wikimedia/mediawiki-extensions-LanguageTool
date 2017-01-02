f3df50e595d74917c2e936b32726069917eca3ab
Umherirrender <umherirrender_de.wp@web.de>
2017-01-02 01:06:04 +0100

    Add grunt-jsonlint
    Together with existing grunt-banana-checker this will check for valid
    i18n files
    
    Change-Id: Id7afe489d9639a2d498417ba90773ea0d1c2f19f

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
@@ -19,10 +20,17 @@ module.exports = function ( grunt ) {
 		},
 		banana: {
 			all: 'i18n/'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
 		}
 	} );
 
-	grunt.registerTask( 'lint', [ 'jshint', 'jscs', 'banana' ] );
+	grunt.registerTask( 'lint', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'test', [ 'lint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
